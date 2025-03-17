9d2e5f1605dd30cc8b967815954a5588a53461fe
andreanavas18 <karennavas333@gmail.com>
2025-03-16 19:08:57 -0500

    fix pdf orden

--- controladores/generarordenpdf.php
+++ controladores/generarordenpdf.php
@@ -19,10 +19,36 @@ $incluirProveedor = $datos["incluirProveedor"] ?? false;
 $incluirRuta = $datos["incluirRuta"] ?? false;
 $incluirObservacion = $datos["incluirObservacion"] ?? false;
 
-$mpdf = new Mpdf();
+$mpdf = new Mpdf([
+    'margin_left' => 10,
+    'margin_right' => 10,
+    'margin_top' => 35,
+    'margin_bottom' => 20,
+    'margin_header' => 10,
+    'margin_footer' => 10
+]);
+
+$css = "
+    body { font-family: Arial, sans-serif; font-size: 12px;  }
+    h1 { text-align: center; color: #00486c; }
+    .tabla { width: 100%; border-collapse: collapse; margin-top: 5px; }
+    .tabla th, .tabla td { border: 1px solid #ddd; padding: 10px; text-align: left; }
+    .tabla th { background-color: #00486c; color: white; }
+    .tabla tr:nth-child(even) { background-color: #f2f2f2; }
+    .tabla tr:hover { background-color: #ddd; }
+";
+$mpdf->WriteHTML($css, \Mpdf\HTMLParserMode::HEADER_CSS);
+
+$logoPath = __DIR__ . '/../assets/images/logonombre.png';
+$logoBase64 = base64_encode(file_get_contents($logoPath));
+$logoHtml = '<div style="text-align: center;"><img src="data:image/png;base64,' . $logoBase64 . '" width="100px"></div>';
+
+$mpdf->SetHTMLHeader($logoHtml);
+
+
 $mpdf->WriteHTML("<h1>Orden de Compra</h1>");
 
-$html = '<table border="1" width="100%" style="border-collapse: collapse;">
+$html = '<table class="tabla">
     <thead><tr>';
 
 if ($incluirProducto) $html .= '<th>Producto</th>';
@@ -38,10 +64,10 @@ foreach ($ordenes as $orden) {
     $html .= "<tr>";
     if ($incluirProducto) $html .= "<td>{$orden['producto']}</td>";
     if ($incluirCantidad) $html .= "<td>{$orden['cantidad']}</td>";
-    if ($incluirCosto) $html .= "<td>{$orden['costo']}</td>";
-    if ($incluirProveedor) $html .= "<td>{$orden['proveedor']}</td>";
-    if ($incluirRuta) $html .= "<td>{$orden['ruta']}</td>";
-    if ($incluirObservacion) $html .= "<td>{$orden['observacion']}</td>";
+    if ($incluirCosto) $html .= "<td>" . (($orden['costo'] != 'null') ? $orden['costo'] : '') . "</td>";
+    if ($incluirProveedor) $html .= "<td>" . (($orden['proveedor'] != 'null') ? $orden['proveedor'] : '') . "</td>";
+    if ($incluirRuta) $html .= "<td>" . (($orden['ruta'] != 'null') ? $orden['ruta'] : '') . "</td>";
+if ($incluirObservacion) $html .= "<td>" . (($orden['observacion'] != 'null') ? $orden['observacion'] : '') . "</td>";
     $html .= "</tr>";
 }
 
