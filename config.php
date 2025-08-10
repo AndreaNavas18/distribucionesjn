de0c9b1ad310a669e4f1f560b4aa4a0a7f58a098
andreanavas18 <karennavas333@gmail.com>
2025-08-10 16:33:20 -0500

    fix config

--- config.php
+++ config.php
@@ -4,8 +4,12 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 require_once __DIR__ . '/vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+
+$dotenvPath = __DIR__ . '/.env';
+if (file_exists($dotenvPath)) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+}
 
 require_once __DIR__ . '/database.php';
 
